fa60bab47db8639141b56486f1fbb103e38b962e
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-06 12:09:44 -0500

    ASP.NET template for GUI.

---
+++ BedrockService/BedrockManagementServiceASP/Program.cs
@@ -0,0 +1,10 @@
+using BedrockManagementServiceASP;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
