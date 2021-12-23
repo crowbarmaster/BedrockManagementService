d6dc4c2f19abdf03cd85d6fd9525bb9934fe5f0b
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-22 22:47:49 -0500

    Adding method for error catching and remove unused method `AppendText()`

--- BedrockService/BedrockService.Shared/Interfaces/IBedrockLogger.cs
+++ BedrockService/BedrockService.Shared/Interfaces/IBedrockLogger.cs
@@ -1,7 +1,10 @@
-﻿namespace BedrockService.Shared.Interfaces {
+﻿using BedrockService.Shared.Classes;
+
+namespace BedrockService.Shared.Interfaces {
     public interface IBedrockLogger {
+        void Initialize();
         void AppendLine(string incomingText);
-        void AppendText(string incomingText);
+        void AppendError(System.Exception exception);
         int Count();
         string FromIndex(int index);
         string ToString();
