908a2f00225a9d6f73c0d4b4a66841381f25e921
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-22 22:25:15 -0500

    Created LogEntry object to assign a timestamp to each log entry in preparation for timestamp-capable logs.

---
+++ BedrockService/BedrockService.Shared/Classes/LogEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockService.Shared.Classes {
+    public class LogEntry {
+        public long TimeStamp { get; set; }
+        public string Text { get; set; }
+
+
+        public LogEntry(string entryText) {
+            Text = entryText;
+            TimeStamp = DateTime.Now.Ticks;
+        }
+    }
+}
