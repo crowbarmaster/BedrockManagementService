b891dc02b829ef20ce32ac6b4772275197e10401
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-24 22:33:15 -0500

    Basic layout created for removing server options.

--- BedrockService/Client/Forms/RemoveServerControl.Designer.cs
+++ BedrockService/Client/Forms/RemoveServerControl.Designer.cs
@@ -24,6 +24,8 @@
         /// </summary>
         private void InitializeComponent() {
             this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -34,11 +36,36 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Select options for server removal, and click \"Confirm\" to proceed.";
             // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Remove player record files",
+            "Remove server\'s backups",
+            "Remove server files",
+            "Remove everything"});
+            this.checkedListBox1.Location = new System.Drawing.Point(28, 76);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(187, 76);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // RemoveServerControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 261);
+            this.ClientSize = new System.Drawing.Size(250, 208);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.label1);
             this.Name = "RemoveServerControl";
             this.Text = "Server removal prompt";
@@ -49,5 +76,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- BedrockService/Client/Forms/RemoveServerControl.cs
+++ BedrockService/Client/Forms/RemoveServerControl.cs
@@ -13,5 +13,9 @@ namespace BedrockService.Client.Forms {
         public RemoveServerControl() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
     }
 }
