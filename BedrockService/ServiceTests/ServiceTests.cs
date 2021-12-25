fc59f791a369f2b78d6515ec04246a05345aa58c
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-24 23:01:02 -0500

    Testing possible tests on service.

--- BedrockService/ServiceTests/ServiceTests.cs
+++ BedrockService/ServiceTests/ServiceTests.cs
@@ -1,18 +1,25 @@
 using BedrockService.Service;
+using BedrockService.Service.Core;
 using BedrockService.Service.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ServiceTests {
     public class ServiceTests {
-        readonly IServiceProvider _services =
-    Program.CreateHostBuilder(new string[] { }).Build().Services; // one liner
+        IHost _host = Program.CreateHostBuilder(new string[] { }).Build();
 
         [Fact]
-        public void VerifyServiceInContainer() {
-            IBedrockService myService = _services.GetRequiredService<IBedrockService>();
+        public void Verify_Service_In_Container() {
+            IBedrockService myService = _host.Services.GetRequiredService<IBedrockService>();
             Assert.NotNull(myService);
+        }
+
+        [Fact]
+        public void Verify_Service_Startup() {
+            _host.Run();
         }
     }
 }
