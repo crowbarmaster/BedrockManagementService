bac4a09c5d77cee221e79d79bcaaf000a7c1cc05
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-12-26 14:13:24 -0500

    Disable Deterministic building to allow automatic versioning, resulting in variable version strings on building.

--- BedrockService/Service/Properties/AssemblyInfo.cs
+++ BedrockService/Service/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.6.0")]
-[assembly: AssemblyFileVersion("2.5.6.0")]
+[assembly: AssemblyVersion("2.56.*")]
+//[assembly: AssemblyFileVersion("2.56.*")]
