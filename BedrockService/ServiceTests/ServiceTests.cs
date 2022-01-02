892a4daa24ade7a21e7b77701ccfa8d32802d5fb
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-01-02 17:23:49 -0500

    Service Tests for startup and shutdown working.

--- BedrockService/ServiceTests/ServiceTests.cs
+++ BedrockService/ServiceTests/ServiceTests.cs
@@ -1,25 +1,59 @@
 using BedrockService.Service;
 using BedrockService.Service.Core;
 using BedrockService.Service.Core.Interfaces;
+using BedrockService.Service.Networking;
+using BedrockService.Service.Networking.MessageInterfaces;
+using BedrockService.Shared.Classes;
+using BedrockService.Shared.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading.Tasks;
 using Xunit;
+using Newtonsoft.Json;
+using System.Text;
+using Xunit.Abstractions;
 
 namespace ServiceTests {
     public class ServiceTests {
-        IHost _host = Program.CreateHostBuilder(new string[] { }).Build();
+        public class TestFixture : IDisposable {
 
-        [Fact]
-        public void Verify_Service_In_Container() {
-            IBedrockService myService = _host.Services.GetRequiredService<IBedrockService>();
-            Assert.NotNull(myService);
+            public static IHost Host = Program.CreateHostBuilder(new string[] { }).Build();
+            public IBedrockService BedrockService = Host.Services.GetRequiredService<IBedrockService>();
+            public JsonSerializerSettings SerializerSettings = new() { TypeNameHandling = TypeNameHandling.All };
+            
+            public TestFixture() {
+                BedrockService.TestStart();
+                while(BedrockService.GetServiceStatus() != ServiceStatus.Started) {
+                    Task.Delay(100).Wait();
+                }
+            }
+
+            public void Dispose() {
+                BedrockService.TestStop();
+                while (BedrockService != null && BedrockService.GetServiceStatus() != ServiceStatus.Stopped) {
+                    Task.Delay(100).Wait();
+                }
+            }
         }
 
-        [Fact]
-        public void Verify_Service_Startup() {
-            _host.Run();
+        public class RunningServiceTests : IClassFixture<TestFixture> {
+            TestFixture _testFixture;
+
+            public RunningServiceTests(TestFixture testFixture) {
+                _testFixture = testFixture;
+            }
+
+            [Fact]
+            public void Verify_Service_Startup() {
+                Assert.True(_testFixture.BedrockService.GetServiceStatus() == ServiceStatus.Started);
+            }
+
+            [Fact]
+            public void Verify_Service_Stop() {
+                _testFixture.Dispose();
+                Assert.True(_testFixture.BedrockService.GetServiceStatus() == ServiceStatus.Stopped);
+            }
         }
     }
 }
