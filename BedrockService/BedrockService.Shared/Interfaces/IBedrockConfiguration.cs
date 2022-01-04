58e7915d4c5be61bd2a99dfd640b59b01e43b4af
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-01-03 21:26:23 -0500

    Clean up extra spaces.

--- BedrockService/BedrockService.Shared/Interfaces/IBedrockConfiguration.cs
+++ BedrockService/BedrockService.Shared/Interfaces/IBedrockConfiguration.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 
 namespace BedrockService.Shared.Interfaces {
     public interface IBedrockConfiguration {
-
         bool InitializeDefaults();
-
         void ProcessConfiguration(string[] configEntries);
-
         bool SetProp(Property propToSet);
-
         Property GetProp(string keyName);
-
         List<Property> GetAllProps();
-
         void SetAllProps(List<Property> newPropList);
-
         List<LogEntry> GetLog();
-
         void SetLog(List<LogEntry> newLog);
     }
 }
--- BedrockService/BedrockService.Shared/Interfaces/IServerConfiguration.cs
+++ BedrockService/BedrockService.Shared/Interfaces/IServerConfiguration.cs
@@ -4,25 +4,15 @@ using System.Collections.Generic;
 namespace BedrockService.Shared.Interfaces {
     public interface IServerConfiguration : IBedrockConfiguration {
         string GetServerName();
-
         string GetFileName();
-
         void AddStartCommand(string command);
-
         bool DeleteStartCommand(string command);
-
         List<StartCmdEntry> GetStartCommands();
-
         void SetStartCommands(List<StartCmdEntry> newEntries);
-
         void AddUpdatePlayer(IPlayer player);
-
         IPlayer GetPlayerByXuid(string xuid);
-
         List<IPlayer> GetPlayerList();
-
         void SetPlayerList(List<IPlayer> newList);
-
         IServerConfiguration GetServerInfo();
     }
 }
