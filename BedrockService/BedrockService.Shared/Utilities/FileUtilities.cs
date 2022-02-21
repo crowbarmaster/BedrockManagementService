7fb1b3385e662559b863728958a8e9a71c015661
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-02-20 20:48:02 -0500

    Minor changes to DeleteFilesFromDirectory. Backup Methods moved to new BackupManager.

--- BedrockService/BedrockService.Shared/Utilities/FileUtilities.cs
+++ BedrockService/BedrockService.Shared/Utilities/FileUtilities.cs
@@ -50,23 +50,29 @@ namespace BedrockService.Shared.Utilities {
                 .ForEach(x => x.CopyTo(Path.Combine(target, x.Name), true));
         }
 
-        public void DeleteFilesRecursively(DirectoryInfo source, bool removeSourceFolder) {
-            var files = source.EnumerateFiles("*", SearchOption.AllDirectories)
-                .ToList();
-            files.ForEach(x => x.Delete());
-            var dirs = source.EnumerateDirectories("*", SearchOption.AllDirectories)
-                .Reverse()
-                .ToList();
-            dirs.ForEach(x => x.Delete());
-            if (removeSourceFolder)
-                source.Delete(true);
+        public Task DeleteFilesFromDirectory(DirectoryInfo source, bool removeSourceFolder) {
+            return Task.Run(() => {
+                var files = source.EnumerateFiles("*", SearchOption.AllDirectories)
+                    .ToList();
+                files.ForEach(x => x.Delete());
+                var dirs = source.EnumerateDirectories("*", SearchOption.AllDirectories)
+                    .Reverse()
+                    .ToList();
+                dirs.ForEach(x => x.Delete());
+                if (removeSourceFolder)
+                    source.Delete(true);
+            });
         }
 
-        public void ClearTempDir() {
-            DirectoryInfo tempDirectory = new($@"{_servicePath}\Temp");
-            if (!tempDirectory.Exists)
-                tempDirectory.Create();
-            DeleteFilesRecursively(tempDirectory, false);
+        public void DeleteFilesFromDirectory(string source, bool removeSourceFolder) => DeleteFilesFromDirectory(new DirectoryInfo(source), removeSourceFolder);
+
+        public Task ClearTempDir() {
+            return Task.Run(() => {
+                DirectoryInfo tempDirectory = new($"{Path.GetTempPath()}\\BMSTemp");
+                if (!tempDirectory.Exists)
+                    tempDirectory.Create();
+                DeleteFilesFromDirectory(tempDirectory, false).Wait();
+            });
         }
 
         public void DeleteFilelist(string[] fileList, string serverPath) {
@@ -85,54 +91,5 @@ namespace BedrockService.Shared.Utilities {
         public void WriteStringToFile(string path, string content) => File.WriteAllText(path, content);
 
         public void WriteStringArrayToFile(string path, string[] content) => File.WriteAllLines(path, content);
-
-        public void CreatePackBackupFiles(string serverPath, string levelName, string targetDirectory) {
-            string resouceFolderPath = $@"{serverPath}\resource_packs";
-            string behaviorFolderPath = $@"{serverPath}\behavior_packs";
-            string behaviorFilePath = $@"{serverPath}\worlds\{levelName}\world_behavior_packs.json";
-            string resoruceFilePath = $@"{serverPath}\worlds\{levelName}\world_resource_packs.json";
-            MinecraftPackParser packParser = new(_processInfo);
-            packParser.ParseDirectory(resouceFolderPath);
-            packParser.ParseDirectory(behaviorFolderPath);
-            WorldPackFileModel worldPacks = new(resoruceFilePath);
-            worldPacks.Contents.AddRange(new WorldPackFileModel(behaviorFilePath).Contents);
-            string packBackupFolderPath = $@"{targetDirectory}\InstalledPacks";
-            Directory.CreateDirectory(packBackupFolderPath);
-            if (worldPacks.Contents.Count > 0) {
-                foreach (WorldPackEntryJsonModel model in worldPacks.Contents) {
-                    MinecraftPackContainer container = packParser.FoundPacks.First(x => x.JsonManifest.header.uuid == model.pack_id);
-                    string packLocation = container.PackContentLocation;
-                    ZipFile.CreateFromDirectory(packLocation, $@"{packBackupFolderPath}\{packLocation[packLocation.LastIndexOf('\\')..]}.zip");
-                }
-            }
-        }
-
-        public Task<bool> BackupWorldFilesFromQuery(Dictionary<string, int> fileNameSizePairs, string worldPath, string destinationPath) {
-            return Task.Run(() => {
-                try {
-                    foreach (KeyValuePair<string, int> file in fileNameSizePairs) {
-                        string fileName = file.Key.Replace('/', '\\');
-                        int fileSize = file.Value;
-                        string filePath = $@"{worldPath}\{fileName}";
-                        string destFilePath = $@"{destinationPath}\{fileName}";
-                        byte[] fileData = null;
-                        using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                        using (MemoryStream ms = new MemoryStream()) {
-                            fs.CopyTo(ms);
-                            ms.Position = 0;
-                            fileData = ms.ToArray();
-                        }
-                        byte[] destData = new byte[fileSize];
-                        Buffer.BlockCopy(fileData, 0, destData, 0, fileSize);
-                        Directory.CreateDirectory(new FileInfo(destFilePath).DirectoryName);
-                        File.WriteAllBytes(destFilePath, fileData);
-                    }
-                    return true;
-                } catch (Exception ex) {
-                    Console.WriteLine($"Error! {ex.Message}");
-                }
-                return false;
-            });
-        }
     }
 }
