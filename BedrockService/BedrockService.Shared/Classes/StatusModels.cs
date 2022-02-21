21568d72fc0659dcbf8382471144972f30dfeb33
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-02-20 20:41:44 -0500

    StatusModels class created as a serializable object containing various service and server status details.

---
+++ BedrockService/BedrockService.Shared/Classes/StatusModels.cs
@@ -0,0 +1,59 @@
+﻿using BedrockService.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum ServiceStatus {
+    Stopped,
+    Starting,
+    Stopping,
+    Started
+}
+
+public enum ServerStatus {
+    Stopped,
+    Starting,
+    Stopping,
+    Started
+}
+
+namespace BedrockService.Shared.Classes {
+    public class StatusUpdateModel {
+        public ServiceStatusModel ServiceStatusModel { get; set; }
+        public ServerStatusModel ServerStatusModel { get; set; }
+    }
+
+    public class ServiceStatusModel {
+        public ServiceStatus ServiceStatus { get; set; }
+        public List<IPlayer> ActivePlayerList { get; set; } = new List<IPlayer>();
+        public DateTime ServiceUptime { get; set; }
+        public int TotalBackups { get; set; }
+        public int TotalBackupSize { get; set; }
+        public string LatestVersion { get; set; }
+    }
+
+    public class ServerStatusModel {
+        public byte ServerIndex { get; set; }
+        public ServerStatus ServerStatus { get; set; }
+        public List<IPlayer> ActivePlayerList { get; set; } = new List<IPlayer>();
+        public int TotalBackups { get; set; }
+        public int TotalSizeOfBackups { get; set; }
+        public string DeployedVersion { get; set; }
+
+        public override bool Equals(object obj) {
+            return obj is ServerStatusModel model &&
+                   ServerStatus == model.ServerStatus &&
+                   ServerIndex == model.ServerIndex &&
+                   TotalBackups == model.TotalBackups &&
+                   TotalSizeOfBackups == model.TotalSizeOfBackups &&
+                   DeployedVersion == model.DeployedVersion;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(ServerStatus, TotalBackups, ServerIndex, TotalSizeOfBackups, DeployedVersion);
+        }
+    }
+}
