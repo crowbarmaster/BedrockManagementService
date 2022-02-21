33da757abab99c8c6fc1842db12d0a586eb2aa0e
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-02-20 20:54:21 -0500

    Remove backup manager from properties editor.

--- BedrockService/Client/Forms/PropEditorForm.cs
+++ BedrockService/Client/Forms/PropEditorForm.cs
@@ -24,7 +24,7 @@ namespace BedrockService.Client.Forms {
             workingProps = propList;
             this.Text = "Properties Editor";
             foreach (Property prop in workingProps) {
-                dataGrid.Rows.Add(new string[2] { prop.KeyName, prop.Value });
+                dataGrid.Rows.Add(new string[2] { prop.KeyName, prop.StringValue });
                 if (prop.KeyName == "server-name" || prop.KeyName == "server-port" || prop.KeyName == "server-portv6") {
                     dataGrid.Rows[index].ReadOnly = true;
                     dataGrid.Rows[index].DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(225, 225, 225);
@@ -44,37 +44,18 @@ namespace BedrockService.Client.Forms {
             }
         }
 
-        public void EnableBackupManager() {
-            this.Text = "Backup Manager";
-            gridView.MultiSelect = true;
-            DelBackupBtn.Enabled = true;
-            DelBackupBtn.Visible = true;
-            SaveBtn.Text = "Rollback this date";
-        }
-
         private void CancelBtn_Click(object sender, EventArgs e) {
             Close();
             Dispose();
         }
 
         private void SaveBtn_Click(object sender, EventArgs e) {
-            if (DelBackupBtn.Enabled) {
-                if (dataGrid.SelectedRows.Count == 0) {
-                    if (dataGrid.SelectedCells.Count == 1) {
-                        dataGrid.SelectedCells[0].OwningRow.Selected = true;
-                    }
-                }
-                if (dataGrid.SelectedRows.Count < 2)
-                    RollbackFolderName = (string)dataGrid.CurrentRow.Cells[0].Value;
-                DialogResult = DialogResult.OK;
-                Close();
-            }
             startCmds = new List<StartCmdEntry>();
             foreach (DataGridViewRow row in dataGrid.Rows) {
                 if (workingProps != null) {
                     foreach (Property prop in workingProps) {
                         if ((string)row.Cells[0].Value == prop.KeyName) {
-                            prop.Value = (string)row.Cells[1].Value;
+                            prop.StringValue = (string)row.Cells[1].Value;
                         }
                     }
                 } else {
@@ -93,30 +74,8 @@ namespace BedrockService.Client.Forms {
             gridView.Refresh();
         }
 
-        private void DelBackupBtn_Click(object sender, EventArgs e) {
-            if (dataGrid.SelectedRows.Count == 0) {
-                if (dataGrid.SelectedCells.Count == 1) {
-                    dataGrid.SelectedCells[0].OwningRow.Selected = true;
-                }
-            }
-            List<string> removeBackups = new List<string>();
-            if (dataGrid.SelectedRows.Count > 0)
-                foreach (DataGridViewRow viewRow in dataGrid.SelectedRows) {
-                    removeBackups.Add((string)viewRow.Cells[0].Value);
-                    dataGrid.Rows.Remove(viewRow);
-                }
-            DelBackupBtn.Enabled = false;
-            BackupBtnEnableAfterDelay();
-            JsonSerializerSettings settings = new() { TypeNameHandling = TypeNameHandling.All };
-            byte[] serializeToBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(removeBackups, Formatting.Indented, settings));
-            FormManager.TCPClient.SendData(serializeToBytes, NetworkMessageSource.Client, NetworkMessageDestination.Service, FormManager.MainWindow.connectedHost.GetServerIndex(FormManager.MainWindow.selectedServer), NetworkMessageTypes.DelBackups);
-        }
-
-        private Task BackupBtnEnableAfterDelay() {
-            return Task.Factory.StartNew(() => {
-                Task.Delay(1500).Wait();
-                Invoke(() => { DelBackupBtn.Enabled = true; });
-            });
+        internal void PopulateBoxes(object p) {
+            throw new NotImplementedException();
         }
     }
 }
