196df4b68afe5c6030f09c3bb4e85f8266f5dc90
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-10-27 16:59:29 -0400

    Updater now fetches latest LiteLoader version from GH.

--- BedrockService/BedrockService.Shared/Classes/Updater.cs
+++ BedrockService/BedrockService.Shared/Classes/Updater.cs
@@ -14,6 +14,7 @@ namespace BedrockService.Shared.Classes {
         private readonly IServiceConfiguration _serviceConfiguration;
         private readonly IProcessInfo _processInfo;
         private string _version;
+        private string _liteLoaderVersion;
 
         public Updater(IProcessInfo processInfo, IBedrockLogger logger, IServiceConfiguration serviceConfiguration) {
             _serviceConfiguration = serviceConfiguration;
@@ -33,25 +34,14 @@ namespace BedrockService.Shared.Classes {
             }
             _version = File.ReadAllText($@"{_processInfo.GetDirectory()}\BmsConfig\latest_bedrock_ver.ini");
             _serviceConfiguration.SetLatestBDSVersion(_version);
+            string[] LLVersion = _serviceConfiguration.GetProp("LatestLiteLoaderVersion").ToString().Split('|');
+            _liteLoaderVersion = LLVersion[1];
         }
 
-        public async Task CheckLatestVersion() {
-            await Task.Run(async () => {
+        public Task CheckLatestVersion() {
+            return Task.Run(() => {
                 _logger.AppendLine("Checking latest BDS Version...");
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/apng,*/*;q=0.8");
-                client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9,en-US;q=0.8");
-                client.DefaultRequestHeaders.Add("Connection", "keep-alive");
-                client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
-                client.DefaultRequestHeaders.Add("Pragma", "no-cache");
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Page Speed Insights) Chrome/27.0.1453 Safari/537.36");
-                client.Timeout = new TimeSpan(0, 0, 3);
-                string content = FetchHTTPContent(client).Result;
-                if (content == null) // This really doesn't fail often. Give it one more try or fail.
-                {
-                    Thread.Sleep(500);
-                    content = await FetchHTTPContent(client);
-                }
+                string content = FetchHTTPContent().Result;
                 if (content == null)
                     return false;
                 Regex regex = new Regex(@"(https://minecraft.azureedge.net/bin-win/bedrock-server-)(.*)(\.zip)", RegexOptions.IgnoreCase);
@@ -62,14 +52,20 @@ namespace BedrockService.Shared.Classes {
                 }
                 string downloadPath = m.Groups[0].Value;
                 string fetchedVersion = m.Groups[2].Value;
-                client.Dispose();
 
                 _logger.AppendLine($"Latest version found: \"{fetchedVersion}\"");
                 if (!File.Exists($@"{_processInfo.GetDirectory()}\BmsConfig\BDSBuilds\BuildArchives\Update_{fetchedVersion}.zip")) {
                     FetchBuild(_processInfo.GetDirectory(), fetchedVersion).Wait();
+                    MinecraftUpdatePackageProcessor packageProcessor = new(_logger, _processInfo, fetchedVersion, $@"{_processInfo.GetDirectory()}\BmsConfig\BDSBuilds\CoreFiles\Build_{fetchedVersion}");
+                    if (!packageProcessor.ExtractCoreFiles()) {
+                        _logger.AppendLine("Error extracting downloaded zip package. Check file/website!");
+                    }
                 }
                 File.WriteAllText($@"{_processInfo.GetDirectory()}\BmsConfig\latest_bedrock_ver.ini", fetchedVersion);
                 _serviceConfiguration.SetLatestBDSVersion(fetchedVersion);
+                if (!File.Exists($@"{_processInfo.GetDirectory()}\BmsConfig\LLBuilds\Update_{_liteLoaderVersion}.zip")) {
+                    FetchLiteLoaderBuild(_processInfo.GetDirectory(), _liteLoaderVersion).Wait();
+                }
                 return true;
             });
         }
@@ -113,8 +109,51 @@ namespace BedrockService.Shared.Classes {
             }
         }
 
-        private async Task<string> FetchHTTPContent(HttpClient client) {
+        public static async Task<bool> FetchLiteLoaderBuild(string servicePath, string version) {
+            string fetchUrl = $"https://github.com/LiteLDev/LiteLoaderBDS/releases/download/{version}/LiteLoader-{version}.zip";
+            string zipPath = $@"{servicePath}\BmsConfig\LLBuilds\Update_{version}.zip";
+            if (!Directory.Exists($@"{servicePath}\BmsConfig\LLBuilds")) {
+                Directory.CreateDirectory($@"{servicePath}\BmsConfig\LLBuilds");
+            }
+            using (var httpClient = new HttpClient()) {
+                using (var request = new HttpRequestMessage(HttpMethod.Get, fetchUrl)) {
+                    DirectoryInfo zipDirInfo = new FileInfo(zipPath).Directory;
+                    zipDirInfo.Create();
+                    using (Stream contentStream = await (await httpClient.SendAsync(request)).Content.ReadAsStreamAsync(), stream = new FileStream(zipPath, FileMode.Create, FileAccess.Write, FileShare.None, 256000, true)) {
+                        try {
+                            if (contentStream.Length > 1000) {
+                                await contentStream.CopyToAsync(stream);
+                            } else {
+                                if (stream != null) {
+                                    stream.Close();
+                                    stream.Dispose();
+                                    File.Delete(zipPath);
+                                }
+                                return false;
+
+                            }
+                        } catch (Exception) {
+                            return false;
+                        }
+                        httpClient.Dispose();
+                        request.Dispose();
+                        contentStream.Dispose();
+                        return true;
+                    }
+                }
+            }
+        }
+
+        private async Task<string> FetchHTTPContent() {
+            HttpClient client = new HttpClient();
             try {
+                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/apng,*/*;q=0.8");
+                client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9,en-US;q=0.8");
+                client.DefaultRequestHeaders.Add("Connection", "keep-alive");
+                client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+                client.DefaultRequestHeaders.Add("Pragma", "no-cache");
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Page Speed Insights) Chrome/27.0.1453 Safari/537.36");
+                client.Timeout = new TimeSpan(0, 0, 3);
                 return await client.GetStringAsync("https://www.minecraft.net/en-us/download/server/bedrock");
             } catch (HttpRequestException) {
                 _logger.AppendLine($"Error! could not fetch current webpage content!");
@@ -122,8 +161,10 @@ namespace BedrockService.Shared.Classes {
                 return null;
             } catch (Exception e) {
                 _logger.AppendLine($"Updater resulted in error: {e.Message}\n{e.InnerException}\n{e.StackTrace}");
+            } finally {
+                client.Dispose();
             }
-            return null;
+                return null;
         }
     }
 }
