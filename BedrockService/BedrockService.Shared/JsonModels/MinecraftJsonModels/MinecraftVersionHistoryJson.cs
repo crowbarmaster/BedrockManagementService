4df7655ab3eb2483092fa0ce4d52796a8e62ab31
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-11-24 13:54:53 -0500

    Custom minecraft version history JSON model.

---
+++ BedrockService/BedrockService.Shared/JsonModels/MinecraftJsonModels/MinecraftVersionHistoryJson.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BedrockService.Shared.JsonModels.MinecraftJsonModels {
+    public class MinecraftVersionHistoryJson {
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+        [JsonPropertyName("bin-win")]
+        public string WindowsBinUrl { get; set; }
+        [JsonPropertyName("bin-lin")]
+        public string LinuxBinUrl { get; set; }
+    }
+}
