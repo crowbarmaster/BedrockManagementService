c992f8b07c2b9ebb18b5f87c8c1b8bc423a0503c
crowbarmaster <crowbarmasterhl2@gmail.com>
2023-02-11 10:25:21 -0500

    Fix Update processor tests to match current needs.

--- BedrockService/ServiceTests/MinecraftUpdatePackageProcessorTests.cs
+++ BedrockService/ServiceTests/MinecraftUpdatePackageProcessorTests.cs
@@ -9,11 +9,12 @@ namespace ServiceTests {
         private readonly DirectoryInfo _directory = new(_testFilePath);
         [Fact]
         public void Can_Create_JsonFiles() {
+            _directory.Create();
+            DirectoryInfo outDir = new DirectoryInfo(_testFilePath + @"\Output");
             IProcessInfo processInfo = new ServiceProcessInfo("TestHost", _directory.FullName, 0, true, true);
             MinecraftUpdatePackageProcessor processor = new(new BedrockLogger(processInfo, new ServiceConfigurator(processInfo)), "1.0", @$"{_testFilePath}\Output");
             processor.ExtractCoreFiles();
             Assert.True(File.Exists(@$"{_testFilePath}\Output\stock_props.conf"));
-            Assert.True(File.Exists(@$"{_testFilePath}\Output\stock_packs.json"));
         }
     }
 }
