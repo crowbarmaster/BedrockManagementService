9dba98da93337f8c4904393b43eea788f46d48cb
crowbarmaster <crowbarmasterhl2@gmail.com>
2023-04-22 18:16:35 -0400

    Updater functionality refactored to utilize newer json manifest system. Stable and robust.

---
+++ BedrockService/BedrockService.Shared/MinecraftJsonModels/MinecraftVersionHistoryJson.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BedrockService.Shared.JsonModels.MinecraftJsonModels {
+    public class MinecraftVersionHistoryJson {
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+        [JsonPropertyName("winurl")]
+        public string WindowsBinUrl { get; set; }
+        [JsonPropertyName("linurl")]
+        public string LinuxBinUrl { get; set; }
+    }
+}
