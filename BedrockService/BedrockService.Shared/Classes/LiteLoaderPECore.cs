3bafc326948b794d2cd04dff4fd9c68e4243caee
crowbarmaster <crowbarmasterhl2@gmail.com>
2023-05-28 10:50:40 -0400

    Update PeProvider to reflect changes made to offical LL software. For safety in service mode, incude working path into functions.

--- BedrockService/BedrockService.Shared/Classes/LiteLoaderPECore.cs
+++ BedrockService/BedrockService.Shared/Classes/LiteLoaderPECore.cs
@@ -5,33 +5,33 @@ using static BedrockService.Shared.Classes.SharedStringBase;
 
 namespace BedrockService.Shared.Classes {
     public static class LiteLoaderPECore {
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessFunctionList@LLPE@@YA_NPEBD@Z")]
-        static extern bool ProcessFunctionList(string pdbFile);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessFunctionList@LLPE@@YA_NPEBD0@Z")]
+        static extern bool ProcessFunctionList(string workingDir, string pdbFile);
 
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?CreateSymbolList@LLPE@@YA_NPEBD0@Z")]
-        static extern bool CreateSymbolList(string symFileName, string pdbFile);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?CreateSymbolList@LLPE@@YA_NPEBD00@Z")]
+        static extern bool CreateSymbolList(string workingDir, string symFileName, string pdbFile);
 
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessLibFile@LLPE@@YA_NPEBD0@Z")]
-        static extern bool ProcessLibFile(string libName, string modExeName);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessLibFile@LLPE@@YA_NPEBD00@Z")]
+        static extern bool ProcessLibFile(string workingDir, string libName, string modExeName);
 
         [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessLibDirectory@LLPE@@YA_NPEBD0@Z")]
         static extern bool ProcessLibDirectory(string directoryName, string modExeName);
 
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessPlugins@LLPE@@YA_NPEBD@Z")]
-        static extern bool ProcessPlugins(string modExeName);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?ProcessPlugins@LLPE@@YA_NPEBD0@Z")]
+        static extern bool ProcessPlugins(string workingDir, string modExeName);
 
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?GenerateDefinitionFiles@LLPE@@YA_NPEBD00@Z")]
-        static extern bool GenerateDefinitionFiles(string pdbName, string apiName, string varName);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?GenerateDefinitionFiles@LLPE@@YA_NPEBD000@Z")]
+        static extern bool GenerateDefinitionFiles(string workingDir, string pdbName, string apiName, string varName);
 
-        [DllImport("LLPeProviderService.dll", EntryPoint = "?CreateModifiedExecutable@LLPE@@YA_NPEBD00@Z")]
-        static extern bool CreateModifiedExecutable(string bedrockName, string liteModName, string pdbName);
+        [DllImport("LLPeProviderService.dll", EntryPoint = "?CreateModifiedExecutable@LLPE@@YA_NPEBD000@Z")]
+        static extern bool CreateModifiedExecutable(string workingDir, string bedrockName, string liteModName, string pdbName);
 
         [DllImport("LLPeProviderService.dll", EntryPoint = "?GetFilteredFunctionListCount@LLPE@@YAHPEBD@Z")]
-        static extern int GetFilteredFunctionListCount(string pdbName);
+        static extern int GetFilteredFunctionListCount(string workingDir, string pdbName);
 
         public static bool FixServerPlugins(IServerConfiguration server) {
-            if (ProcessLibFile($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\LiteLoader.dll", $@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\{server.GetSettingsProp(ServerPropertyKeys.ServerExeName)}")) {
-                FixPluginsRecusively(new DirectoryInfo($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\plugins"), $@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\{server.GetSettingsProp(ServerPropertyKeys.ServerExeName)}");
+            if (ProcessLibFile($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}", $@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\LiteLoader.dll", server.GetSettingsProp(ServerPropertyKeys.ServerExeName).StringValue)) {
+                FixPluginsRecusively(new DirectoryInfo($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\plugins"), server.GetSettingsProp(ServerPropertyKeys.ServerExeName).StringValue);
                 return true;
             }
             return false;
@@ -40,13 +40,13 @@ namespace BedrockService.Shared.Classes {
         public static void FixPluginsRecusively(DirectoryInfo directory, string liteModExeName) {
             ProcessLibDirectory(directory.FullName, liteModExeName);
             foreach (DirectoryInfo dir in directory.GetDirectories()) {
-                ProcessLibDirectory(dir.FullName, liteModExeName);
+                ProcessLibDirectory(directory.FullName, liteModExeName);
                 FixPluginsRecusively(dir, liteModExeName);
             }
         }
 
-        public static bool BuildLLExe(IServerConfiguration server, string liteVersion) {
-            if (CreateModifiedExecutable($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\bedrock_server.exe", $@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\{server.GetSettingsProp(ServerPropertyKeys.ServerExeName)}", $@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}\bedrock_server.pdb") &&
+        public static bool BuildLLExe(IServerConfiguration server) {
+            if (CreateModifiedExecutable($@"{server.GetSettingsProp(ServerPropertyKeys.ServerPath)}", "bedrock_server.exe", server.GetSettingsProp(ServerPropertyKeys.ServerExeName).StringValue, "bedrock_server.pdb") &&
                 FixServerPlugins(server)) {
                 return true;
             }
--- BedrockService/LLPeProviderService/LLPeProviderService/peEdit.cpp
+++ BedrockService/LLPeProviderService/LLPeProviderService/peEdit.cpp
@@ -54,6 +54,7 @@ namespace LLPE {
 		std::string bedrockPdbName = pdbName != "" ? pdbName : "bedrock_server.pdb";
 		if (FunctionList.size() == 0) {
 			FunctionList = *loadPDB(str2wstr(pdbName).c_str());
+			std::cout << "[LLPeService] Loaded " << FunctionList.size() << " functions from PDB file." << std::endl;
 			if (FunctionList.size() == 0) {
 				std::cout << "[Error] Could not open PDB!" << std::endl;
 				std::cout << "PDB name: " << pdbName << std::endl;
@@ -116,36 +117,37 @@ namespace LLPE {
 				FilteredFunctionList.push_back(fn);
 			}
 		}
+			std::cout << "[LLPeService] Kept " << FilteredFunctionList.size() << " symbols." << std::endl;
 			return true;
 		}
 		return true;
 	}
 
-	bool LLPEAPI ProcessFunctionList(const char* pdbFile = "") {
+	bool LLPEAPI ProcessFunctionList(const char* workingDir, const char* pdbFile = "") {
 		std::string pdbStr = std::string(pdbFile);
-		if (!InitializeFunctionList(pdbStr, "")) {
+		if (!InitializeFunctionList(std::string(workingDir).append("\\").append(pdbStr), "")) {
 			std::cout << "[Error][LLPeService] Error processing function list from PDB!" << std::endl;
 			return false;
 		}
 		return true;
 	}
 
-	bool LLPEAPI CreateSymbolList(const char* symFileName, const char* pdbFile) {
-		if (!InitializeFunctionList(pdbFile, symFileName, true)) {
+	bool LLPEAPI CreateSymbolList(const char* workingDir, const char* symFileName, const char* pdbFile) {
+		if (!InitializeFunctionList(std::string(workingDir).append("\\").append(pdbFile), std::string(workingDir).append("\\").append(symFileName), true)) {
 			std::cout << "[Error][LLPeService] Error processing function list from PDB!" << std::endl;
 			return false;
 		}
 		return true;
 	}
 
-	bool LLPEAPI ProcessLibFile(const char* libName, const char* modExeName) {
+	bool LLPEAPI ProcessLibFile(const char* workingDir, const char* libPath, const char* modExeName) {
 		std::string LiteModExe = modExeName != "" ? std::string(modExeName) : "bedrock_server_mod.exe";
 		std::ifstream libFile;
 		std::ofstream outLibFile;
 
-		libFile.open(libName, std::ios::in | std::ios::binary);
+		libFile.open(libPath, std::ios::in | std::ios::binary);
 		if (!libFile) {
-			std::cout << "[Error] Cannot open " << libName << std::endl;
+			std::cout << "[Error] Cannot open " << libPath << std::endl;
 			return false;
 		}
 		bool saveFlag = false;
@@ -155,7 +157,7 @@ namespace LLPE {
 			for (int i = 0; i < imports.size(); i++) {
 				if (imports[i].get_name() == "bedrock_server_mod.exe") {
 					imports[i].set_name(std::string(LiteModExe));
-					std::cout << "[LLPeService] Modding dll file " << libName << std::endl;
+					std::cout << "[LLPeService] Modding dll file " << libPath << std::endl;
 					saveFlag = true;
 				}
 			}
@@ -168,17 +170,17 @@ namespace LLPE {
 				section& attachedImportedSection = LiteLib_PE->add_section(ImportSection);
 				pe_bliss::rebuild_imports(*LiteLib_PE, imports, attachedImportedSection, import_rebuilder_settings(true, false));
 
-				outLibFile.open("LiteLoader.tmp", std::ios::out | std::ios::binary | std::ios::trunc);
+				outLibFile.open(std::string(workingDir).append("\\").append("LiteLoader.tmp"), std::ios::out | std::ios::binary | std::ios::trunc);
 				if (!outLibFile) {
 					std::cout << "[Error] Cannot create LiteLoader.tmp!" << std::endl;
 					return false;
 				}
-				std::cout << "[LLPeService] Writting dll file " << libName << std::endl;
+				std::cout << "[LLPeService] Writting dll file " << libPath << std::endl;
 				pe_bliss::rebuild_pe(*LiteLib_PE, outLibFile);
 				libFile.close();
-				std::filesystem::remove(std::filesystem::path(libName));
+				std::filesystem::remove(std::filesystem::path(libPath));
 				outLibFile.close();
-				std::filesystem::rename(std::filesystem::path("LiteLoader.tmp"), std::filesystem::path(libName));
+				std::filesystem::rename(std::filesystem::path(std::string(workingDir).append("\\").append("LiteLoader.tmp")), libPath);
 			}
 			if (libFile.is_open()) libFile.close();
 			if (outLibFile.is_open()) outLibFile.close();
@@ -195,6 +197,9 @@ namespace LLPE {
 		std::filesystem::directory_iterator directory(directoryName);
 
 		for (auto& file : directory) {
+			if (file.is_directory()) {
+				ProcessLibDirectory(file.path().string().c_str(), modExeName);
+			}
 			if (!file.is_regular_file()) {
 				continue;
 			}
@@ -204,45 +209,43 @@ namespace LLPE {
 			std::string name = reinterpret_cast<std::string&>(u8Name);
 			std::string ext = reinterpret_cast<std::string&>(u8Ext);
 
-			// Check is dll
+			// Check if dll
 			if (ext != ".dll") {
 				continue;
 			}
 
-			std::string pluginName;
-			pluginName.append(directoryName).append("\\").append(name);
-			if (!ProcessLibFile(pluginName.c_str(), LiteModExe.c_str())) return false;
+			if (!ProcessLibFile(file.path().parent_path().string().c_str(), path.string().c_str(), LiteModExe.c_str())) return false;
 		}
 		return true;
 	}
 
-	bool LLPEAPI ProcessPlugins(const char* modExeName) {
+	bool LLPEAPI ProcessPlugins(const char* workingDir, const char* modExeName) {
 		std::string LiteModExeName = modExeName != "" ? modExeName : "bedrock_server_mod.exe";
-		if (ProcessLibFile("LiteLoader.dll", modExeName) && ProcessLibDirectory("plugins", modExeName) && ProcessLibDirectory("plugins\\LiteLoader", modExeName)) {
+		if (ProcessLibFile(workingDir, std::string(workingDir).append("\\").append("LiteLoader.dll").c_str(), modExeName) && ProcessLibDirectory("plugins", modExeName)) {
 			return true;
 		}
 		return false;
 	}
 
-	bool LLPEAPI GenerateDefinitionFiles(const char* pdbName, const char* apiName, const char* varName) {
+	bool LLPEAPI GenerateDefinitionFiles(const char* workingDir, const char* pdbName, const char* apiName, const char* varName) {
 		std::cout << "[LLPeService] Generating definition files... Please wait." << std::endl;
 		std::string ApiFileName = apiName != "" ? std::string(apiName) : "bedrock_server_api.def";
 		std::string VarFileName = varName != "" ? std::string(varName) : "bedrock_server_var.def";
 		std::string PdbFileName = pdbName != "" ? std::string(pdbName) : "bedrock_server.pdb";
 		std::ofstream BDSDef_API;
 		std::ofstream BDSDef_VAR;
-		if(!InitializeFunctionList(PdbFileName, "")) {
+		if(!InitializeFunctionList(std::string(workingDir).append("\\").append(PdbFileName), "")) {
 			return false;
 		}
 
-		BDSDef_API.open(ApiFileName, std::ios::ate | std::ios::out);
+		BDSDef_API.open(std::string(workingDir).append("\\").append(ApiFileName), std::ios::ate | std::ios::out);
 		if (!BDSDef_API) {
 			std::cout << "[Error][LLPeService] Cannot create bedrock_server_api.def" << std::endl;
 			return false;
 		}
 		BDSDef_API << "LIBRARY bedrock_server.dll\nEXPORTS\n";
 
-		BDSDef_VAR.open(VarFileName, std::ios::ate | std::ios::out);
+		BDSDef_VAR.open(std::string(workingDir).append("\\").append(VarFileName), std::ios::ate | std::ios::out);
 		if (!BDSDef_VAR) {
 			std::cout << "[Error][LLPeService] Failed to create " << VarFileName << std::endl;
 			return false;
@@ -269,48 +272,71 @@ namespace LLPE {
 		return true;
 	}
 
-	bool LLPEAPI CreateModifiedExecutable(const char* bedrockName, const char* liteModName, const char* pdbName) {
+	bool LLPEAPI CreateModifiedExecutable(const char* workingDir, const char* bedrockName, const char* liteModName, const char* pdbName) {
 		std::string bedrockExeName = bedrockName != "" ? bedrockName : "bedrock_server.exe";
 		std::string liteModExeName = liteModName != "" ? liteModName : "bedrock_server_mod.exe";
 		std::string bedrockPdbName = pdbName != "" ? pdbName : "bedrock_server.pdb";
 		std::ifstream             OriginalBDS;
 		std::ofstream             ModifiedBDS;
-		pe_base* OriginalBDS_PE = nullptr;
-		pe_base* LiteLib_PE = nullptr;
-		export_info                OriginalBDS_ExportInf;
-		exported_functions_list         OriginalBDS_ExportFunc;
+		pe_base*				  OriginalBDS_PE = nullptr;
+		pe_base*                  LiteLib_PE = nullptr;
+		export_info               OriginalBDS_ExportInf;
+		exported_functions_list   OriginalBDS_ExportFunc;
 		uint16_t                  ExportLimit = 0;
 		int                       ExportCount = 1;
 		int                       ApiDefCount = 1;
 		int                       ApiDefFileCount = 1;
+		std::string				  preloaderDllSelected = "LLPreloader.dll";
+		std::string				  preloaderSymSelected = "dlsym_real";
+		if (std::filesystem::exists(std::string(workingDir).append("\\LLPreLoader.dll").c_str())) {
+			std::cout << "[LLPeService] Detected Liteloader 2.XX" << std::endl;
+		}
+		else {
+			std::cout << "[LLPeService] Detected Liteloader 3.XX" << std::endl;
+			preloaderDllSelected = "Preloader.dll";
+			preloaderSymSelected = "pl_resolve_symbol";
+		}
 
-		if (!InitializeFunctionList(bedrockPdbName, "")) {
+		if (!InitializeFunctionList(std::string(workingDir).append("\\").append(bedrockPdbName), "")) {
 			return false;
 		}
-		OriginalBDS.open(bedrockExeName, std::ios::in | std::ios::binary);
+		OriginalBDS.open(std::string(workingDir).append("\\").append(bedrockExeName), std::ios::in | std::ios::binary);
 		if (!OriginalBDS) {
 			std::cout << "[Error][LLPeService] Cannot open bedrock_server.exe" << std::endl;
 			return false;
 		}
-		ModifiedBDS.open(liteModExeName, std::ios::out | std::ios::binary | std::ios::trunc);
+		ModifiedBDS.open(std::string(workingDir).append("\\").append(liteModExeName), std::ios::out | std::ios::binary | std::ios::trunc);
 		if (!ModifiedBDS) {
 			std::cout << "[Error][LLPeService] Cannot open " << LiteModExeName << std::endl;
 			return false;
 		}
 
+		std::cout << "[LLPeService] Open and modify BDS executable file..." << std::endl;
+		std::unordered_set<std::string> funcsWithNames;
+
 		OriginalBDS_PE = new pe_base(pe_factory::create_pe(OriginalBDS));
 		try {
 			OriginalBDS_ExportFunc = get_exported_functions(*OriginalBDS_PE, OriginalBDS_ExportInf);
+			for (const auto& fn : OriginalBDS_ExportFunc) {
+				if (fn.has_name()) {
+					funcsWithNames.insert(fn.get_name());
+				}
+			}
 		}
 		catch (const pe_exception& e) {
 			std::cout << "[Error][pe_bliss] " << e.what() << std::endl;
 			return false;
 		}
 		ExportLimit = get_export_ordinal_limits(OriginalBDS_ExportFunc).second;
-
+		int dupeCount = 0;
+		int nonFuncCount = 0;
 		for (const auto& fn : FilteredFunctionList) {
 			try {
 				if (!fn.IsFunction) {
+					if (std::count(funcsWithNames.begin(), funcsWithNames.end(), fn.Name)) {
+						dupeCount++;
+						continue;
+					}
 					exported_function func;
 					func.set_name(fn.Name);
 					func.set_rva(fn.Rva);
@@ -322,6 +348,9 @@ namespace LLPE {
 					}
 					OriginalBDS_ExportFunc.push_back(func);
 				}
+				else {
+					nonFuncCount++;
+				}
 			}
 			catch (const pe_exception& e) {
 				std::cout << "[Error][pe_bliss] " << e.what() << std::endl;
@@ -332,10 +361,12 @@ namespace LLPE {
 				return false;
 			}
 		}
+		std::cout << "[LLPeService] Skipped " << nonFuncCount << " non-functions, and removed " << dupeCount << " duplicates pulled from PDB." << std::endl;
+		std::cout << "[LLPeService] Insert sections into executable..." << std::endl;
 		try {
 			section ExportSection;
 			ExportSection.get_raw_data().resize(1);
-			ExportSection.set_name("ExpFunc");
+			ExportSection.set_name(".nexp");
 			ExportSection.readable(true);
 			section& attachedExportedSection = OriginalBDS_PE->add_section(ExportSection);
 			rebuild_exports(*OriginalBDS_PE, OriginalBDS_ExportInf, OriginalBDS_ExportFunc, attachedExportedSection);
@@ -343,10 +374,10 @@ namespace LLPE {
 			imported_functions_list imports(get_imported_functions(*OriginalBDS_PE));
 
 			import_library preLoader;
-			preLoader.set_name("LLPreLoader.dll");
+			preLoader.set_name(preloaderDllSelected);
 
 			imported_function func;
-			func.set_name("dlsym_real");
+			func.set_name(preloaderSymSelected);
 			func.set_iat_va(0x1);
 
 			preLoader.add_import(func);
@@ -380,10 +411,10 @@ namespace LLPE {
 		return true;
 	}
 
-	int LLPEAPI GetFilteredFunctionListCount(const char* pdbName) {
+	int LLPEAPI GetFilteredFunctionListCount(const char* workingDir, const char* pdbName) {
 		std::string bedrockPdbName = pdbName != "" ? pdbName : "bedrock_server.pdb";
 		if (FilteredFunctionList.size() == 0) {
-			if (InitializeFunctionList(pdbName, "")) {
+			if (InitializeFunctionList(std::string(workingDir).append("\\").append(pdbName), "")) {
 				return 0;
 			}
 		}
