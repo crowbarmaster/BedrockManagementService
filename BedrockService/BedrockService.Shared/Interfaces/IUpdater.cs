c6d95562acc58b170ed7eb80a124b67855b0e2b7
crowbarmaster <crowbarmasterhl2@gmail.com>
2023-08-21 18:51:02 -0400

    Update GetVersionList to return a list of SimpleVersionModels instead of a string.

--- BedrockService/BedrockService.Shared/Interfaces/IUpdater.cs
+++ BedrockService/BedrockService.Shared/Interfaces/IUpdater.cs
@@ -1,14 +1,15 @@
-﻿using BedrockService.Shared.JsonModels.LiteLoaderJsonModels;
+﻿using BedrockService.Shared.Classes;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BedrockService.Shared.Interfaces {
     public interface IUpdater {
         void Initialize();
+        void SetNewLogger(IServerLogger logger);
         Task CheckLatestVersion();
         string GetBaseVersion(string version);
         Task<bool> FetchBuild(string version);
         Task ReplaceServerBuild(string versionOverride = "");
-        List<string> GetVersionList();
+        List<SimpleVersionModel> GetVersionList();
     }
 }
