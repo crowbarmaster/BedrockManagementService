e42fd68ca3c4de43b1f499a2016221f211a779de
Tad Tarrant <69368213+crowbarmaster@users.noreply.github.com>
2024-02-10 13:40:32 -0500

    Create manifest model for Exports/Imports.

---
+++ MinecraftService/MinecraftService.Shared/SerializeModels/ExportImportManifestModel.cs
@@ -0,0 +1,15 @@
+﻿using MinecraftService.Shared.Classes;
+
+namespace MinecraftService.Shared.SerializeModels {
+    public class ExportImportManifestModel {
+        public string Filename;
+        public FileTypeFlags FileType;
+        public PackageFlags PackageFlags;
+
+        public ExportImportManifestModel (ExportImportFileModel exportImportFile) {
+            Filename = exportImportFile.Filename;
+            FileType = exportImportFile.FileType;
+            PackageFlags = exportImportFile.PackageFlags;
+        }
+    }
+}
