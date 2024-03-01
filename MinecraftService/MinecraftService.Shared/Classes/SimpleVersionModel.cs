47c4d4bae0d334f5fefe08b268dcf46df795a8a5
Tad Tarrant <69368213+crowbarmaster@users.noreply.github.com>
2024-02-29 20:37:36 -0500

    Generated Equals(Object).

--- MinecraftService/MinecraftService.Shared/Classes/SimpleVersionModel.cs
+++ MinecraftService/MinecraftService.Shared/Classes/SimpleVersionModel.cs
@@ -1,5 +1,8 @@
-﻿namespace MinecraftService.Shared.Classes {
-    public class SimpleVersionModel {
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinecraftService.Shared.Classes {
+    public class SimpleVersionModel : IEquatable<SimpleVersionModel> {
         public string Version { get; set; }
         public bool IsBeta { get; set; }
 
@@ -10,6 +13,24 @@
 
         public override string ToString() {
             return Version;
+        }
+
+        public override bool Equals(object obj) {
+            return Equals(obj as SimpleVersionModel);
+        }
+
+        public bool Equals(SimpleVersionModel other) {
+            return other is not null &&
+                   Version == other.Version &&
+                   IsBeta == other.IsBeta;
+        }
+
+        public static bool operator ==(SimpleVersionModel left, SimpleVersionModel right) {
+            return EqualityComparer<SimpleVersionModel>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(SimpleVersionModel left, SimpleVersionModel right) {
+            return !(left == right);
         }
     }
 }
