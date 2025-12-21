773136a6ae0ea66018fc949eddd25dd775bbb526
Tad Tarrant <69368213+crowbarmaster@users.noreply.github.com>
2025-12-20 21:13:26 -0500

    Fix RemovePack logic.

--- MinecraftService/Service/Networking/NetworkStrategies/RemovePack.cs
+++ MinecraftService/Service/Networking/NetworkStrategies/RemovePack.cs
@@ -3,19 +3,19 @@ using MinecraftService.Shared.Classes.Networking;
 using MinecraftService.Shared.Classes.Service;
 using MinecraftService.Shared.Classes.Service.Configuration;
 using MinecraftService.Shared.Classes.Service.Core;
+using MinecraftService.Shared.FileModels.MinecraftFileModels;
 using MinecraftService.Shared.PackParser;
 using Newtonsoft.Json;
 using System.Text;
 using System.Text.Json;
 using static MinecraftService.Shared.Classes.Service.Core.SharedStringBase;
 
-namespace MinecraftService.Service.Networking.NetworkStrategies
-{
+namespace MinecraftService.Service.Networking.NetworkStrategies {
     public class RemovePack(ServiceConfigurator serviceConfiguration, MmsLogger logger) : IMessageParser {
 
         public Message ParseMessage(Message message) {
             string stringData = Encoding.UTF8.GetString(message.Data);
-            string pathToWorldFolder = $@"{serviceConfiguration.GetServerInfoByIndex(message.ServerIndex).GetSettingsProp(ServerPropertyKeys.ServerPath)}\worlds\{serviceConfiguration.GetServerInfoByIndex(message.ServerIndex).GetProp(MmsDependServerPropKeys.LevelName)}";
+            string worldDirectory = $@"{serviceConfiguration.GetServerInfoByIndex(message.ServerIndex).GetSettingsProp(ServerPropertyKeys.ServerPath)}\worlds\{serviceConfiguration.GetServerInfoByIndex(message.ServerIndex).GetProp(MmsDependServerPropKeys.LevelName)}";
             List<MinecraftPackContainer>? container = JsonConvert.DeserializeObject<List<MinecraftPackContainer>>(stringData, SharedStringBase.GlobalJsonSerialierSettings);
             foreach (MinecraftPackContainer content in container) {
                 MinecraftFileUtilities.RemovePackFromServer(serviceConfiguration.GetServerInfoByIndex(message.ServerIndex), content);
